f124e497ce602aeb0ab44fc4d406c9daf4642b60
Aman kumar <108401753+Amankumar968@users.noreply.github.com>
2024-05-18 21:45:56 +0530

    host on server

--- db.js
+++ db.js
@@ -8,10 +8,11 @@ if (!mongoURL) {
     throw new Error('MONGODB_URL is not defined');
 }
 
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+
+mongoose.connect('mongodb://localhost:27017/mydatabase')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
 
 const db = mongoose.connection;
 
