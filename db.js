01abae9e9809d9bfb11a22a4edf86db1c97e447f
Aman kumar <108401753+Amankumar968@users.noreply.github.com>
2024-05-20 10:46:11 +0530

    Error Fix

--- db.js
+++ db.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const mongoURL=process.env.MONGODB_URL_LOCAL
 const mongoURL = process.env.MONGODB_URL;
 
-if (!mongoURL) {
-    throw new Error('MONGODB_URL is not defined');
-}
 
 
-mongoose.connect('mongodb://localhost:27017/mydatabase')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+
+mongoose.connect(mongoURL,{
+
+useNewUrlParser:true,
+useUnifiedTopology:true
+})
 
 
 const db = mongoose.connection;
